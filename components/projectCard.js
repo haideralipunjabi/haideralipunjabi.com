2356238fd23727472d376e7194dc75e5fb11d86d
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-09-30 19:09:41 +0530

    ui changes

--- components/projectCard.js
+++ components/projectCard.js
@@ -1,29 +1,33 @@
-import styles from "./projectCard.module.scss"
+import styles from "./projectCard.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
-import parse, { attributesToProps, domToReact } from "html-react-parser"
+import parse, { attributesToProps, domToReact } from "html-react-parser";
 
 const options = {
-    replace: ({name,attribs,children}) => {
-        if(name==='a' && attribs && attribs.href){
-            const props = attributesToProps(attribs);
-            return <a {...props} target="_blank" rel="noopener noreferrer">{domToReact(children)}</a>
-        }
-        if(name==='p'){
-          return domToReact(children)
-        }
+  replace: ({ name, attribs, children }) => {
+    if (name === "a" && attribs && attribs.href) {
+      const props = attributesToProps(attribs);
+      return (
+        <a {...props} target="_blank" rel="noopener noreferrer">
+          {domToReact(children)}
+        </a>
+      );
     }
-}
+    if (name === "p") {
+      return domToReact(children);
+    }
+  },
+};
 export default function ProjectCard({ project, className }) {
   return (
-    <div className="card is-flex is-flex-direction-column is-justify-content-space-between" style={{ height: "100%"}}>
+    <div className="card" style={{ height: "100%" }}>
       <header className="card-header">
         <p className="card-header-title">{project.title}</p>
       </header>
       <div className="card-content">
-        <div className="content" style={{overflowWrap:"anywhere"}}>
-            {parse(project.content,options)}
-            <div className={classNames("tags mt-2",styles.tags)}>
+        <div className="content" style={{ overflowWrap: "anywhere" }}>
+          {parse(project.content, options)}
+          {/* <div className={classNames("tags mt-2",styles.tags)}>
             {
                 project.metadata.tags.map(tag=>
                     <span key={tag.icon.split(",")[1]} class="tag is-rounded has-text-weight-semibold" data-type={tag.icon.split(",")[1]}>
@@ -32,28 +36,31 @@ export default function ProjectCard({ project, className }) {
                     </span>
                 )
             }
-            </div>
+            </div> */}
+          <div className="is-flex is-justify-content-space-around is-flex-wrap-wrap">
+            {project.metadata.buttons.map((button) => (
+              <a
+                href={button.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button mb-2"
+                key={button.name}
+                atl={button.name}
+              >
+                <span className="icon">
+                  <FontAwesomeIcon icon={button.icon.split(",")} />
+                </span>
+                <span>{button.name}</span>
+              </a>
+            ))}
+          </div>
         </div>
       </div>
-      <footer
+      {/* <footer
         className="card-footer"
       >
-        {project.metadata.buttons.map((button) => (
-          <a
-            href={button.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card-footer-item"
-            key={button.name}
-            atl={button.name}
-          >
-            <span className="icon">
-              <FontAwesomeIcon icon={button.icon.split(",")} />
-            </span>
-            <span>{button.name}</span>
-          </a>
-        ))}
-      </footer>
+        
+      </footer> */}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Navbar from "../components/navbar";
-import {getBlogData, getData, getGithubData} from "../lib/api";
+import { getBlogData, getData, getGithubData } from "../lib/api";
 import Main from "../sections/main";
 import Projects from "../sections/projects";
 import Skills from "../sections/skills";
@@ -8,29 +8,34 @@ import Press from "../sections/press";
 import Footer from "../components/footer";
 import { useEffect, useState } from "react";
 export default function Home(props) {
-  const {socials,skills, projects, githubData,blogData,pressData} = props;
-  const [activeSection,setActiveSection] = useState(null);
+  const { socials, skills, projects, githubData, blogData, pressData } = props;
+  const [activeSection, setActiveSection] = useState(null);
 
   return (
     <>
-    <Navbar active={activeSection}/>
-    <Main socials={socials}  navbarEvent={setActiveSection}/>
-    <Skills skills={skills}  navbarEvent={setActiveSection}/>
-    <Projects projects={projects} githubData={githubData} navbarEvent={setActiveSection}/>
-    <Blog blogData={blogData} navbarEvent={setActiveSection}/>
-    <Press pressData={pressData} navbarEvent={setActiveSection}/>
-    <Footer/>
+      <Navbar active={activeSection} />
+      <Main socials={socials} navbarEvent={setActiveSection} />
+      <Skills skills={skills} navbarEvent={setActiveSection} />
+      <Projects
+        projects={projects}
+        githubData={githubData}
+        navbarEvent={setActiveSection}
+      />
+      <Blog blogData={blogData} navbarEvent={setActiveSection} />
+      <Press pressData={pressData} navbarEvent={setActiveSection} />
+      <Footer />
     </>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
-  const socials = (await getData("socials")) || []
-  const skills = (await getData("skills")) || []
-  const projects = (await getData("projects")) || []
-  const pressData = (await getData("inpress")) || []
-  const githubData = (await getGithubData()) || {}
-  const blogData = (await getBlogData()) || []
+  const socials = (await getData("socials")) || [];
+  const skills = (await getData("skills")) || [];
+  const projects = (await getData("projects")) || [];
+  const pressData = (await getData("inpress")) || [];
+  const githubData = (await getGithubData()) || {};
+  // const githubData = {};
+  const blogData = (await getBlogData()) || [];
 
   return {
     props: {
@@ -39,7 +44,7 @@ export async function getStaticProps(context) {
       projects,
       githubData,
       blogData,
-      pressData
-    }
-  }
+      pressData,
+    },
+  };
 }
