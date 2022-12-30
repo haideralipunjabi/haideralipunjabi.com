d799d1c4d4727a8324d75d1e9d21286f911c7f00
Haider Ali Punjabi <haiderali@cyberservices.com>
2022-12-30 22:44:35 +0530

    Fixed issue with unoptimizedImages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const APP_DIR = process.env.APP_DIR == "true";
 module.exports = {
   reactStrictMode: true,
-  pageExtensions: process.env.APP_DIR == 'true'
+  pageExtensions: APP_DIR
     ? ["jsx", "js", "tsx", "ts"]
     : ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  images: {
+    unoptimized: !APP_DIR,
+  },
   experimental: {
-    appDir: process.env.APP_DIR == 'true',
+    appDir: APP_DIR,
   },
 };
